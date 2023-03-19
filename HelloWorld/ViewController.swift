bc7877b538d8aeb3a7346486a236aaa6fbb4985f
yarik <ylimanov@mail.ru>
2023-03-19 14:35:04 +0700

    View Controller commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var button: UIButton!
+    
+    private var currentLight = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.layer.cornerRadius = 10
+        redLightView.layer.cornerRadius = 75
+        yellowLightView.layer.cornerRadius = 75
+        greenLightView.layer.cornerRadius = 75
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
 
-
+    @IBAction func startButtonTapped(_ sender: Any) {
+        if currentLight == 0 {
+            redLightView.alpha = 1.0
+            greenLightView.alpha = 0.3
+            button.setTitle("NEXT", for: .normal)
+            currentLight = 1
+        } else if currentLight == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1.0
+            currentLight = 2
+        } else if currentLight == 2 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1.0
+            currentLight = 0
+        }
+    }
 }
 
